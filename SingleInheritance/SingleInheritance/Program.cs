c52fdb9a8ab0eb3d34a0735984ede86212a0c1b9
Muhammad-Ohee <md.ohee86@gmail.com>
2024-01-30 14:05:35 +0600

    example added

---
+++ SingleInheritance/SingleInheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿//Console.WriteLine("Hello, OS");
+
+namespace SingleInheritance
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuboid obj = new Cuboid(4, 4, 4);
+
+            Console.WriteLine($"Rectangle Area: {obj.Area()}");
+            Console.WriteLine($"Rectangle Perimeter: {obj.Perimeter()}");
+            Console.WriteLine($"Cuboid Volume: {obj.Volume()}");
+
+        }
+    }
+
+    // Parent Class
+    public class Rectangle
+    {
+        public int length;
+        public int breadth;
+
+        public int Area()
+        {
+            return length * breadth;
+        }
+
+        public int Perimeter()
+        {
+            return 2 * (length +  breadth);
+        }
+    }
+
+    // Child Class
+    public class Cuboid : Rectangle
+    {
+        public int height;
+
+        public Cuboid(int l, int b, int h)
+        {
+            length = l;
+            breadth = b;
+            this.height = h;
+        }
+
+        public int Volume()
+        {
+            return height * breadth * length ;
+        }
+    }
+}
---
+++ UseOfBaseKeyword/UseOfBaseKeyword/Program.cs
@@ -0,0 +1,46 @@
+﻿//Console.WriteLine("Hello, OS");
+
+namespace UseOfBaseKeyword
+{
+    
+    class A
+    {
+        public A (int number)
+        {
+            Console.WriteLine($"A Class constructor is called : {number}");
+        }
+    }
+
+    class B : A
+    {
+        public B () : base(4)
+        {
+            Console.WriteLine($"B Class constructor is called");
+        }
+
+        
+    }
+
+    // How to pass dynamic value to Parent class constructor in C#
+    class C
+    {
+        public C (int number)
+        {
+            Console.WriteLine($"Class C Constructor is called : {number}");
+        }
+    }
+
+    class D : C
+    {
+        public D (int number) : base(number)
+        {
+            Console.WriteLine($"Class D Constructor is called : {number}");
+        }
+
+        static void Main(string[] args)
+        {
+            B b = new B();
+            D d = new D(4);
+        }
+    }
+}
